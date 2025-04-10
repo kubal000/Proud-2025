37c987b2ea303a5772c0e264bb1f45824b93455e
kubal000 <jakub.las@email.cz>
2025-04-10 17:42:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'tajny_klic'
 socketio = SocketIO(app, async_mode='eventlet')
-session_data = {}
+
+usernames = {}
+sid_to_username = {}
 
 @app.route('/')
 def index():
@@ -22,6 +24,36 @@ def soutez():
         return redirect(url_for('index'))  # Pokud není uživatel přihlášen, přesměruj na login
     return render_template('soutez.html', username=session_data['username'])
 
+@socketio.on('connect')
+def handle_connect():
+    print('Uživatel připojen:', request.sid)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    sid = request.sid
+    username = sid_to_username.pop(sid, None)
+    if username and username in usernames:
+        del usernames[username]
+    print('Odpojeno:', sid)
+
+@socketio.on('send_message')
+def handle_send_message(data):
+    sender_sid = request.sid
+    sender_name = sid_to_username.get(sender_sid, "neznámý")
+    target_user = data.get('target_user')
+    message = data.get('message')
+
+    target_sid = usernames.get(target_user)
+    if target_sid:
+        emit('receive_message', {'sender': sender_name, 'message': message}, to=target_sid)
+
+@socketio.on('register_username')
+def handle_register_username(data):
+    username = data.get('username')
+    if username:
+        usernames[username] = request.sid
+        sid_to_username[request.sid] = username
+        print(f'{username} přihlášen jako {request.sid}')
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=10000)
