10ad28de3387c84673e20a9a7c81ca39b49d882c
kubal000 <jakub.las@email.cz>
2025-04-10 17:35:16 +0200

    Update funkce.js

--- static/funkce.js
+++ static/funkce.js
@@ -3,25 +3,29 @@ function Login() {
     document.querySelectorAll("input[type='text']").forEach(input => {
         data[input.name] = input.value;
     });
-    if (data.heslo == "Proud2025") {
-        location = "/login?username="+data.username
-    }
-    else {
+
+    if (data.heslo === "Proud2025") {
+        location.href = "/login?username=" + encodeURIComponent(data.username);
+    } else {
         document.getElementById("upozorneni").style.display = "block";
-    } 
+    }
 }
-var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+var socket = io.connect(window.location.protocol + '//' + window.location.host);
 
 function sendMessage() {
-    var targetUser = document.getElementById("target_user").value;
-    var message = document.getElementById("message").value;
-    
-    socket.emit('send_message', { 'target_user': targetUser, 'message': message });
+    const targetUser = document.getElementById("target_user").value;
+    const message = document.getElementById("message").value;
+
+    socket.emit('send_message', {
+        target_user: targetUser,
+        message: message
+    });
 }
 
 socket.on('receive_message', function(data) {
-    var messages = document.getElementById("messages");
-    var messageItem = document.createElement("li");
+    const messages = document.getElementById("messages");
+    const messageItem = document.createElement("li");
     messageItem.textContent = data.sender + ": " + data.message;
     messages.appendChild(messageItem);
 });
