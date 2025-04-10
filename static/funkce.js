43bf9f5874a003787d24c763b9c91825e7b35187
kubal000 <jakub.las@email.cz>
2025-04-10 13:33:19 +0200

    Update funkce.js

--- static/funkce.js
+++ static/funkce.js
@@ -1,3 +1,7 @@
 function Login() {
+    let data = {};
+    document.querySelectorAll("input[type='number']").forEach(input => {
+        data[input.name] = input.value;
+    });
   location = "/login?username="+data.username
 }
