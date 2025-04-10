3b368816001318377800dc1b0e046b94e5a239ba
kubal000 <jakub.las@email.cz>
2025-04-10 16:32:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ def index():
 @app.route('/login', methods=['GET'])
 def login():
     username = request.args('username')
-    session['username'] = username  # Uložení jména do session
-    return redirect(url_for('soutez'))
+    #session['username'] = username  # Uložení jména do session
+    return render_template('soutez.html', username=username) #redirect(url_for('soutez'))
 
 @app.route('/soutez')
 def soutez():
