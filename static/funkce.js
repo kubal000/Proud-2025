d921eea7e3aacdc8f64a5b77e484da011baf8005
kubal000 <jakub.las@email.cz>
2025-04-10 13:45:50 +0200

    Update funkce.js

--- static/funkce.js
+++ static/funkce.js
@@ -4,7 +4,8 @@ function Login() {
         data[input.name] = input.value;
     });
     if (data.heslo == "Proud2025") {
-        location = "/login?username="+data.username
+        //location = "/login?username="+data.username
+        alert("Přihlásil ses. Super!!!")
     }
     else {
         document.GetElementByID("upozorneni").style.display("block")
